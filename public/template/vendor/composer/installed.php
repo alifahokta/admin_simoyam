e7ca0f2cb234575f680f9f77ace123ba8d3cb8d6
alifahokta <alifahokta1@gmail.com>
2023-05-16 20:00:54 +0700

    Admin Template

---
+++ public/template/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a6a1fd3341db9c8b6f0b34b225b83e963f2e8444',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a6a1fd3341db9c8b6f0b34b225b83e963f2e8444',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
