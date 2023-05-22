fe4d4fc2193b7d4afcef52645a38604778621323
merthaindri <merthaindri12@gmail.com>
2023-05-22 11:44:56 +0700

    Routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin/login_page');
+    return view('admin/first_page');
 });
 
 Route::get('/login', function () {
